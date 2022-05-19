08083359c76e21eccf2df346218ee5eab1ec2caf
Melina Perraut <melina@perraut.us>
2022-05-19 13:49:22 -0700

    Add Navbar and logo image

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,18 @@
 import { Link, Outlet } from "react-router-dom";
+import NavBar from './Nav';
 
 export default function App() {
     return (
-        <div>
-            <h1>Dev Deck</h1>
-            {/* @TODO: nav bar here */}
-            <nav>
+        // <div>
+        <div className="appContainer">
+            {/* <h1>DevDeck</h1> */}
+            <NavBar />
+            {/* <nav>
                 <Link to="/home">Home</Link> |{" "}
                 <Link to="/profile">Profile</Link> |{" "}
                 <Link to="/project">Project Details</Link>
             </nav>
-            <Outlet />
+            <Outlet /> */}
         </div>
     )
 }
---
+++ client/src/Nav.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { BrowserRouter as Route, Link, NavLink } from 'react-router-dom';
+import {
+    Navbar,
+    Nav,
+} from 'react-bootstrap';
+
+function NavBar() {
+    return (
+        <Navbar bg="custom" variant="dark" expand="sm" sticky="top" className="nav-bar">
+            <Navbar.Brand as={Link} exact to="/" className="logo"><img
+                src="imgs/Logo.png"
+                height="45"
+                width="35"
+                className="mr-3"
+                alt="DevDeck Logo"
+            />DevDeck</Navbar.Brand>
+            {/* <Navbar.Toggle aria-controls="basic-navbar-nav" /> */}
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="mr-auto">
+                    <Nav.Link as={NavLink} exact to="/" activeClassName="active">Home</Nav.Link>
+                    <Nav.Link as={NavLink} to="/profile" activeClassName="active">Profile</Nav.Link>
+                    <Nav.Link as={NavLink} to="/about" activeClassName="active">About</Nav.Link>
+                </Nav>
+                {/* <Navbar.Brand as={Link} exact to="/profile" className="nav-profile-pic"><img
+                    src="img/default-profile.jpg"
+                    height="45"
+                    width="45"
+                    className="mr-3 profile-pic-navbar"
+                    alt="User's profile"
+                /></Navbar.Brand> */}
+            </Navbar.Collapse>
+        </Navbar>
+    )
+}
+
+export default NavBar;
