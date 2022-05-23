c3671cc1e7a8adb072699a568ff1ff2484c7311e
Melina Perraut <melina@perraut.us>
2022-05-23 15:04:47 -0700

    Make edits to navbar

--- client/src/Nav.js
+++ client/src/Nav.js
@@ -7,8 +7,9 @@ import {
 
 function NavBar() {
     return (
-        <Navbar bg="custom" variant="dark" expand="sm" sticky="top" className="nav-bar">
-            <Navbar.Brand as={Link} exact to="/" className="logo"><img
+        <Navbar bg="custom" variant="dark" expand="sm" sticky="top" className="navbar">
+            <Navbar.Brand as={Link} exact to="/" className="logo">
+            <img
                 src="imgs/DevDeck_card.png"
                 height="45"
                 width="35"
@@ -18,11 +19,13 @@ function NavBar() {
             {/* <Navbar.Toggle aria-controls="basic-navbar-nav" /> */}
             {/* <Navbar.Collapse id="basic-navbar-nav"> */}
                 <Nav className="mr-auto">
-                    <Nav.Link as={NavLink} exact to="/" activeClassName="active">Home</Nav.Link>
-                    <Nav.Link as={NavLink} to="/profile" activeClassName="active">Profile</Nav.Link>
-                    <Nav.Link as={NavLink} to="/project" activeClassName="active">Project Details</Nav.Link>
+                    <Nav.Link as={NavLink} exact to="/">Home</Nav.Link>
+                    <Nav.Link as={NavLink} to="/profile">Profile</Nav.Link>
+                    <Nav.Link as={NavLink} to="/project">Project Details</Nav.Link>
+                    {/* activeClassName="active" */}
                 </Nav>
-                <Navbar.Brand as={Link} exact to="/profile" className="nav-profile-pic"><img
+                <Navbar.Brand as={Link} exact to="/profile" className="nav-profile-pic">
+                <img
                     src="imgs/profile_default.png"                    
                     height="35"
                     width="35"
