7bbb1cf7e7708853c67917a37b511b6a15d29242
Melina Perraut <melina@perraut.us>
2022-05-23 10:47:24 -0700

    Work on navbar and layout of Project Details page

--- client/src/Footer.js
+++ client/src/Footer.js
@@ -5,7 +5,7 @@ import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 function Footer() {
     return (
-        <div className="pt-5">
+        // <div className="pt-5">
             <footer className="footer">
                 <div className="container text-center">
                     <span className="contact-info">© 2022 Melina Perraut, Max Bennett, Elbert Cheng, Susan Yang - <a
@@ -13,7 +13,7 @@ function Footer() {
                         aria-label="Link to GitHub page" className="github-icon"><FontAwesomeIcon icon={faGithub} /></a> </span>
                 </div>
             </footer>
-        </div>
+        // </div>
     )
 }
 
--- client/src/routes/ProjectDetails.js
+++ client/src/routes/ProjectDetails.js
@@ -1,7 +1,27 @@
 export default function ProjectDetails() {
     return (
         <div>
-            <p>This is the project details page.</p>
+            {/* <p>This is the project details page.</p> */}
+            <h1>Project Title</h1>
+            <div className="thumbnail">
+                <img src="imgs/thumbnail_default.png" alt="project thumbnail"></img>
+            </div>
+            <p>Blurb</p>
+            <h2>Inspiration</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <h2>What it does</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <h2>How I built it</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <h2>Challenges I ran into</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <h2>Accomplishments I'm proud of</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <h2>What I learned</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <h2>What's next</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+
         </div>
     )
 }
