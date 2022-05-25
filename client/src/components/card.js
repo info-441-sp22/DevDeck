482517e50c825dd2e488be811c51069cd3f791a3
maxmiles <maxmiles@uw.edu>
2022-05-25 16:54:58 -0700

    fixed css on cards sorta

--- client/src/components/card.js
+++ client/src/components/card.js
@@ -3,7 +3,7 @@ import { Button } from "reactstrap";
 
 export function Card() {
     return (
-        <div className="card" style={{width: '18em'}}>
+        <div className="card" style={{width: '18em', margin: '1rem'}}>
         <img className="card-img-top" src="..." alt="Project thumbnail"></img>
         <div className="card-body">
             <h5 className="card-title">Card title</h5>
