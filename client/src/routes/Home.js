8c4571edcb2489f6c2c1405e19f54cb4cb2f9cab
Melina Perraut <melina@perraut.us>
2022-05-26 17:07:54 -0700

    Edit blurb box

--- client/src/routes/Home.js
+++ client/src/routes/Home.js
@@ -9,12 +9,16 @@ export default function HomePage() {
                 <div className="row welcome">
                     <div className="row">
                         {/* <!--"Blurb Box" Start--> */}
-                        <div className="blurb-box position-relative text-left bg-blurb p-3 m-sm-3 mb-sm-4" aria-label="person with laptop" style={{
-                            backgroundImage: `url("imgs/blurb_background.png")`
+                        <div className="blurb-box bg-blurb p-3 m-sm-3 mb-sm-4" aria-label="person with laptop" style={{
+                            backgroundImage: `url("imgs/blurb_background2.jpg")`
                         }}>
+                            {/* position-relative text-left  */}
                             <div className="blurb-text pl-lg-4 mb-0 my-4">
-                                <h1 className="display-5 font-weight-bold">Welcome to DevDeck!</h1>
-                                <p className="font-weight-normal lead">We are a platform for developers to share their projects with the world. Etc... </p>
+                                <h1 className="font-weight-bold">Welcome to DevDeck!</h1>
+                                <p className="font-weight-normal ">We are a platform for developers to share their projects with the world. 
+                                We enable our users to attract collaborators and potential sponsors through their project posts. 
+                                Every day, a new development project is created and does not get the exposure it deserves. </p>
+                                <p><em>So play your cards right with DevDeck, and show the world your winning hand.</em></p>
                             </div>
                         </div>
                         {/* <!--"Blurb Box" End--> */}
