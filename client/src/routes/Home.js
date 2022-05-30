a2f80f479be6d8acf2ead3c5b1236102b0eee558
Melina Perraut <melina@perraut.us>
2022-05-29 17:52:03 -0700

    Update Home page

--- client/src/routes/Home.js
+++ client/src/routes/Home.js
@@ -10,14 +10,14 @@ export default function HomePage() {
                     <div className="row">
                         {/* <!--"Blurb Box" Start--> */}
                         <div className="blurb-box bg-blurb p-3 m-sm-3 mb-sm-4" aria-label="person with laptop" style={{
-                            backgroundImage: `url("imgs/blurb_background2.jpg")`
+                            backgroundImage: `url("imgs/blurb_background3.jpg")`
                         }}>
                             {/* position-relative text-left  */}
                             <div className="blurb-text pl-lg-4 mb-0 my-4">
                                 <h1 className="font-weight-bold">Welcome to DevDeck!</h1>
-                                <p className="font-weight-normal ">We are a platform for developers to share their projects with the world. 
-                                We enable our users to attract collaborators and potential sponsors through their project posts. 
-                                Every day, a new development project is created and does not get the exposure it deserves. </p>
+                                <p className="font-weight-normal ">We are a platform for developers to share their projects with the world.
+                                    We enable our users to attract collaborators and potential sponsors through their project posts.
+                                    Every day, a new development project is created and does not get the exposure it deserves. </p>
                                 <p><em>So play your cards right with DevDeck, and show the world your winning hand.</em></p>
                             </div>
                         </div>
@@ -25,14 +25,12 @@ export default function HomePage() {
                     </div>
                     <div class="col">
                         <div className="featured-project">
-                            {/* featured prokect card here */}
-                            <h2>Featured project title:</h2>
-                            <h5>This is a great project so it is featured.
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                                sed do eiusmod tempor incididunt ut labore et dolore
-                                magna aliqua. Ut enim ad minim veniam, quis nostrud
-                                exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                                consequat.</h5>
+                            <h2>Featured projects:</h2>
+                            <div class="row">
+                                <Card />
+                                <Card />
+                                <Card />
+                            </div>
                         </div>
                     </div>
                 </div>
