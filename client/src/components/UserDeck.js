be78a93759e4e088faaffa42cba0c2a091d4d65a
Melina Perraut <melina@perraut.us>
2022-05-31 15:04:04 -0700

    Make edits to Card and UserDeck components

--- client/src/components/UserDeck.js
+++ client/src/components/UserDeck.js
@@ -6,14 +6,64 @@
 import React, { useState, useEffect } from "react"; //import React Component
 import { Button } from "reactstrap";
 
-export default function UserDeck(props) {
+function UserDeck(props) {
     const [val, setVal] = useState("");
 
-    console.log(props.username)
+    console.log("Username", props.username); // username is undefined 
+    // findPosts();
 
     return (        
         <div>            
-            <Button color="primary">Test</Button>
+            <Button color="primary">Test UserDeck</Button>
         </div>
     )
 }
+
+// async function findPosts() {
+//     let postsJSON = await fetchJSON(`api/posts?username=${props.username}`)
+//     console.log(postsJSON);
+// }
+
+export default UserDeck;
+
+/*
+router.get('/', async function (req, res) {
+    try {
+        var username = req.query.username;
+        let toReturn = [];
+
+        // find all posts in mongodb database
+        let allPosts = await req.models.Post.find();
+
+        // for each of the urls, generate the html preview of the webpage
+        for (let i = 0; i < allPosts.length; i++) {
+            let post = allPosts[i];
+            let preview = await getURLPreview(post.url);
+            let newPost = {
+                "description": post.description,
+                "htmlPreview": preview,
+                "username": post.username, // A5: added username
+                "likes": post.likes, // A6: added likes
+                "type": post.type, // creative component
+                "id": post._id, // Mongodb built-in "_id" field with the key "id"
+                "created_date": post.created_date
+            };
+
+            if (username) { // if query parameter is set
+                if (post.username === username) {
+                    toReturn.push(newPost); // send back just posts made by user
+                }
+            } else {
+                toReturn.push(newPost); // send back all posts
+            }
+        }
+
+        // return an array of json objects with their htmlPreviews and descriptions
+        res.json(toReturn);
+
+    } catch (error) { // catch errors
+        console.log(error);
+        res.status(500).json({ "status": "error", "error": error });
+    }
+});
+*/
--- client/src/components/card.js
+++ client/src/components/card.js
@@ -8,13 +8,13 @@ export function Card(props) {
         <div className="card bg-custom" style={{ width: '18em', margin: '1rem' }}>
             <img className="card-img-top" src="..." alt="Project thumbnail"></img>
             <div className="card-body">
-                {/* TODO: Title of project => link to project page */}
                 <h5 className="card-title">{props.title}</h5>
                 <p className="card-text">{props.blurb}</p>
-                {/* add in all props fields/project info */}
+                {/* add in necessary props fields/project info -> only include title & blurb in card */}
                 <Button className="btn btn-primary" onClick={() => console.log("Link")}>
                     View project details
                 </Button>
+                {/* need to create new page for each card's project details */}
             </div>            
         </div>
     )
