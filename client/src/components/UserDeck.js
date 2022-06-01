682afc6f1565af6ab9bedd2df515b19b22801ee9
Melina Perraut <melina@perraut.us>
2022-05-31 22:50:05 -0700

    Work on creating project cards for each user's projects

--- client/src/components/UserDeck.js
+++ client/src/components/UserDeck.js
@@ -1,72 +1,57 @@
 // UserDeck should contain the project cards that the user has made
 // Accepts props with username
 // Need to create Cards based on post fields (created in CreateProjectModal): 
-// title, blurb, longer_description, url_link, collaborators
+// title, blurb, longer_description, url_link, tech_stack, collaborators
 
 import React, { useState, useEffect } from "react"; //import React Component
-import { Button } from "reactstrap";
+// import { Button } from "reactstrap";
+import { Card } from "../components/Card.js";
+import { fetchJSON } from '../utils/utils.js';
+import { BASEPOINT } from "../App";
 
 function UserDeck(props) {
-    const [val, setVal] = useState("");
+    // const [val, setVal] = useState("");
+    let userPosts = [];
 
-    console.log("Username", props.username); // username is undefined 
-    // findPosts();
+    // let postsJSON = findPosts(props)
+    findPosts(props)
+        .then(data => {
+            // console.log(data)
+            for (let i = 0; i < data.length; i++) {
+                let post = data[i];
+                // console.log(post);
+                userPosts.push(<Card
+                    key={i}
+                    post={post}
+                    username={props.username}>
+                </Card>);
+            }
+            // console.log("user posts", userPosts);
+        })
+
+    console.log("user posts", userPosts);
+    // document.getElementById("testspan").innerHTML = userPosts;
+
+    let testTitle = "Test title";
 
     return (
-        <div>            
-            <Button color="primary">Test UserDeck</Button>
+        <div>
+            {userPosts}
+            {/* {userPosts.map((postCard) => <div>{postCard}</div>)} */}
 
-            {/* <Card title={title}></Card> */}
+            {/* <Button color="primary" onClick={() => console.log("Click")}>Test UserDeck</Button> */}
+            {/* <span id="testspan"></span> */}
+
+            <Card title={testTitle}></Card>
         </div>
-        
+
     )
 }
 
-// async function findPosts() {
-//     let postsJSON = await fetchJSON(`api/posts?username=${props.username}`)
-//     console.log(postsJSON);
-// }
+// call api/posts GET endpoint, filtering for posts created by user
+async function findPosts(props) {
+    let postsJSON = await fetchJSON(BASEPOINT + `/api/posts?username=${props.username}`);
+    return postsJSON;
+}
 
 export default UserDeck;
-
-/*
-router.get('/', async function (req, res) {
-    try {
-        var username = req.query.username;
-        let toReturn = [];
-
-        // find all posts in mongodb database
-        let allPosts = await req.models.Post.find();
-
-        // for each of the urls, generate the html preview of the webpage
-        for (let i = 0; i < allPosts.length; i++) {
-            let post = allPosts[i];
-            let preview = await getURLPreview(post.url);
-            let newPost = {
-                "description": post.description,
-                "htmlPreview": preview,
-                "username": post.username, // A5: added username
-                "likes": post.likes, // A6: added likes
-                "type": post.type, // creative component
-                "id": post._id, // Mongodb built-in "_id" field with the key "id"
-                "created_date": post.created_date
-            };
-
-            if (username) { // if query parameter is set
-                if (post.username === username) {
-                    toReturn.push(newPost); // send back just posts made by user
-                }
-            } else {
-                toReturn.push(newPost); // send back all posts
-            }
-        }
-
-        // return an array of json objects with their htmlPreviews and descriptions
-        res.json(toReturn);
-
-    } catch (error) { // catch errors
-        console.log(error);
-        res.status(500).json({ "status": "error", "error": error });
-    }
-});
-*/
